928934a1ebd0f01399e8accd562c6d6116163211
yoshix3 <y.nara.2000+github@gmail.com>
2022-06-04 17:54:28 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-njskxz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-njskxz)
---
+++ index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { store } from './src/app/store';
+import { Provider } from 'react-redux';
+
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
